c2dbfff3e03ac3b186f3df43f1278c76d6370517
Hugo Visser <hugo@littlerobots.nl>
2015-05-19 15:44:41 +0200

    Add copyright notices

--- tools/src/main/java/nl/littlerobots/cupboard/tools/provider/UriHelper.java
+++ tools/src/main/java/nl/littlerobots/cupboard/tools/provider/UriHelper.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2015 Little Robots
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package nl.littlerobots.cupboard.tools.provider;
 
 import android.annotation.SuppressLint;
@@ -18,36 +34,64 @@ import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 public class UriHelper {
     private final UriMatcher mMatcher;
     private final String mAuthority;
-    private int mRuleCount = 0;
     private final List<Class<?>> mEntities = new ArrayList<Class<?>>(20);
     private final List<String> mPaths = new ArrayList<String>(20);
     private final Uri mBaseUri;
+    private int mRuleCount = 0;
 
-
-    public static class AuthorityBuilder {
-        private List<Class<?>> mEntities;
-
-        private AuthorityBuilder(Collection<Class<?>> entities) {
-            mEntities = new ArrayList<>(entities);
-        }
-
-        public UriHelper forBaseUri(Uri baseUri) {
-            if (!"content://".equals(baseUri.getScheme())) {
-                throw new IllegalArgumentException("scheme should be content://");
-            }
-            return new UriHelper(baseUri, mEntities);
-        }
-
-        public UriHelper forAuthority(String authority) {
-            return new UriHelper(Uri.parse("content://"+authority), mEntities);
-        }
-    }
 
     private UriHelper(Uri baseUri, List<Class<?>> entities) {
         mAuthority = baseUri.getAuthority();
         mMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         mBaseUri = baseUri;
         addAll(entities);
+    }
+
+    /**
+     * Create an {@link nl.littlerobots.cupboard.tools.provider.UriHelper.AuthorityBuilder} for the supplied {@link nl.qbusict.cupboard.Cupboard} instance
+     *
+     * @param cupboard the instance
+     * @return the builder
+     */
+    public static AuthorityBuilder with(Cupboard cupboard) {
+        return new AuthorityBuilder(cupboard.getRegisteredEntities());
+    }
+
+    /**
+     * Create a helper for the supplied base uri. The instance returned by {@link nl.qbusict.cupboard.CupboardFactory#cupboard()} is used for
+     * retrieving the registered entities
+     *
+     * @param baseUri the base uri
+     * @return a new UriHelper
+     */
+    public static UriHelper with(Uri baseUri) {
+        return new AuthorityBuilder(cupboard().getRegisteredEntities()).forBaseUri(baseUri);
+    }
+
+    /**
+     * Create a helper for the supplied authority. The instance returned by {@link nl.qbusict.cupboard.CupboardFactory#cupboard()} is used for
+     * retrieving the registered entities
+     *
+     * @param authority the authority
+     * @return a new UriHelper
+     */
+    public static UriHelper with(String authority) {
+        return new AuthorityBuilder(cupboard().getRegisteredEntities()).forAuthority(authority);
+    }
+
+    /**
+     * Create a helper for the supplied authority and path. The instance returned by {@link nl.qbusict.cupboard.CupboardFactory#cupboard()} is used for
+     * retrieving the registered entities
+     *
+     * @param authority the authority
+     * @param path      the path prefix
+     * @return a new UriHelper
+     */
+    public static UriHelper with(String authority, String path) {
+        if (!path.startsWith("/")) {
+            path = "/" + path;
+        }
+        return new AuthorityBuilder(cupboard().getRegisteredEntities()).forBaseUri(Uri.parse("content://" + authority + path));
     }
 
     private void addAll(Collection<Class<?>> entities) {
@@ -64,10 +108,10 @@ public class UriHelper {
     private void add(Class<?> clz, String path) {
         int index = mEntities.indexOf(clz);
         if (index > -1) {
-            throw new IllegalStateException("Entity "+clz+" already added.");
+            throw new IllegalStateException("Entity "+clz + " already added.");
         }
         mMatcher.addURI(mAuthority, path, mRuleCount++);
-        mMatcher.addURI(mAuthority, path+"/#", mRuleCount++);
+        mMatcher.addURI(mAuthority, path + "/#", mRuleCount++);
         mEntities.add(clz);
         mPaths.add(path);
     }
@@ -83,7 +127,7 @@ public class UriHelper {
         if (index > UriMatcher.NO_MATCH) {
             return mEntities.get(index / 2);
         }
-        throw new IllegalArgumentException("Uri "+uri.toString()+" does not match any class");
+        throw new IllegalArgumentException("Uri " + uri.toString()+" does not match any class");
     }
 
     /**
@@ -95,9 +139,9 @@ public class UriHelper {
     public Uri getUri(Class<?> clz) {
         int index = mEntities.indexOf(clz);
         if (index > -1) {
-            return Uri.parse("content://"+mAuthority+"/"+mPaths.get(index));
+            return Uri.parse("content://" + mAuthority + "/" + mPaths.get(index));
         }
-        throw new IllegalArgumentException("Class "+clz.getName()+" cannot be mapped to an uri");
+        throw new IllegalArgumentException("Class " + clz.getName()+" cannot be mapped to an uri");
     }
 
     /**
@@ -130,47 +174,23 @@ public class UriHelper {
         return mBaseUri;
     }
 
-    /**
-     * Create an {@link nl.littlerobots.cupboard.tools.provider.UriHelper.AuthorityBuilder} for the supplied {@link nl.qbusict.cupboard.Cupboard} instance
-     * @param cupboard the instance
-     * @return the builder
-     */
-    public static AuthorityBuilder with(Cupboard cupboard) {
-        return new AuthorityBuilder(cupboard.getRegisteredEntities());
-    }
+    public static class AuthorityBuilder {
+        private List<Class<?>> mEntities;
 
-    /**
-     * Create a helper for the supplied base uri. The instance returned by {@link nl.qbusict.cupboard.CupboardFactory#cupboard()} is used for
-     * retrieving the registered entities
-     * @param baseUri the base uri
-     * @return a new UriHelper
-     */
-    public static UriHelper with(Uri baseUri) {
-        return new AuthorityBuilder(cupboard().getRegisteredEntities()).forBaseUri(baseUri);
-    }
-
-    /**
-     * Create a helper for the supplied authority. The instance returned by {@link nl.qbusict.cupboard.CupboardFactory#cupboard()} is used for
-     * retrieving the registered entities
-     * @param authority the authority
-     * @return a new UriHelper
-     */
-    public static UriHelper with(String authority) {
-        return new AuthorityBuilder(cupboard().getRegisteredEntities()).forAuthority(authority);
-    }
-
-    /**
-     * Create a helper for the supplied authority and path. The instance returned by {@link nl.qbusict.cupboard.CupboardFactory#cupboard()} is used for
-     * retrieving the registered entities
-     * @param authority the authority
-     * @param path the path prefix
-     * @return a new UriHelper
-     */
-    public static UriHelper with(String authority, String path) {
-        if (!path.startsWith("/")) {
-            path="/"+path;
+        private AuthorityBuilder(Collection<Class<?>> entities) {
+            mEntities = new ArrayList<>(entities);
         }
-        return new AuthorityBuilder(cupboard().getRegisteredEntities()).forBaseUri(Uri.parse("content://"+authority+path));
+
+        public UriHelper forBaseUri(Uri baseUri) {
+            if (!"content://".equals(baseUri.getScheme())) {
+                throw new IllegalArgumentException("scheme should be content://");
+            }
+            return new UriHelper(baseUri, mEntities);
+        }
+
+        public UriHelper forAuthority(String authority) {
+            return new UriHelper(Uri.parse("content://" +authority), mEntities);
+        }
     }
 
 }
