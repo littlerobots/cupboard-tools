f133ac361eea4fb9184e11a19d04b3d9284c27f8
Hugo Visser <hugo@littlerobots.nl>
2015-05-21 09:09:23 +0200

    Add basic example

---
+++ example/src/main/java/nl/littlerobots/example/ExampleActivity.java
@@ -0,0 +1,84 @@
+/*
+ *    Copyright 2015 Little Robots
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package nl.littlerobots.example;
+
+import android.app.ListActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import nl.littlerobots.cupboard.tools.provider.UriHelper;
+import nl.littlerobots.example.model.Cheese;
+import nl.littlerobots.example.provider.MyProvider;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+public class ExampleActivity extends ListActivity implements LoaderCallbacks<Cursor> {
+
+    private CheeseAdapter mAdapter;
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        CursorLoader loader = new CursorLoader(this);
+        loader.setUri(UriHelper.with(MyProvider.AUTHORITY).getUri(Cheese.class));
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new CheeseAdapter(this);
+        setListAdapter(mAdapter);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    private static class CheeseAdapter extends CursorAdapter {
+
+        public CheeseAdapter(Context context) {
+            super(context, null, 0);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            Cheese cheese = cupboard().withCursor(cursor).get(Cheese.class);
+            ((TextView) view).setText(cheese.name);
+        }
+    }
+}
---
+++ example/src/main/java/nl/littlerobots/example/provider/MyProvider.java
@@ -0,0 +1,93 @@
+/*
+ *    Copyright 2015 Hugo Visser (Little Robots)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package nl.littlerobots.example.provider;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+import nl.littlerobots.cupboard.tools.convert.ListFieldConverterFactory;
+import nl.littlerobots.cupboard.tools.provider.CupboardContentProvider;
+import nl.littlerobots.example.BuildConfig;
+import nl.littlerobots.example.model.Cheese;
+import nl.littlerobots.example.model.Plateau;
+import nl.qbusict.cupboard.CupboardBuilder;
+import nl.qbusict.cupboard.CupboardFactory;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+public class MyProvider extends CupboardContentProvider {
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider";
+
+    static {
+        // register a ListFieldConverterFactory that will serialize any List<> to json in the database
+        // using Gson
+        CupboardFactory.setCupboard(new CupboardBuilder().
+                registerFieldConverterFactory(new ListFieldConverterFactory(new Gson())).build());
+        cupboard().register(Cheese.class);
+        cupboard().register(Plateau.class);
+    }
+
+    public MyProvider() {
+        super(AUTHORITY, 1);
+    }
+
+    @Override
+    protected void onCreateDatabase(SQLiteDatabase db) {
+        super.onCreateDatabase(db);
+        String[] names = {"Gouda", "Brie", "Grevé", "Roquefort"};
+
+        db.beginTransaction();
+        try {
+            for (String name : names) {
+                Cheese cheese = new Cheese();
+                cheese.name = name;
+                cupboard().withDatabase(db).put(cheese);
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+
+        Plateau dutchPlate = new Plateau();
+        dutchPlate.cheeses = new ArrayList<>(2);
+        Cheese gouda = new Cheese();
+        gouda.name = "Gouda";
+        Cheese edammer = new Cheese();
+        edammer.name = "Edammer";
+        dutchPlate.cheeses.add(gouda);
+        dutchPlate.cheeses.add(edammer);
+
+        // note that the collections of cheeses in the cheeses array
+        // won't be stored as separate entities here. They will be
+        // embedded in the Plateau entity that is stored as json
+        cupboard().withDatabase(db).put(dutchPlate);
+    }
+
+    @Override
+    protected Uri insertInTransaction(Uri uri, ContentValues values) {
+        if (mUriHelper.getMatchedClass(uri) == Cheese.class && mUriHelper.isCollection(uri)) {
+            values.put("sync_date", System.currentTimeMillis());
+
+        }
+        return super.insertInTransaction(uri, values);
+    }
+}
