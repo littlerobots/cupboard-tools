d379ad217d735ddc8394716aea4684ae9d3eef60
Hugo Visser <hugo@littlerobots.nl>
2015-05-21 09:11:39 +0200

    Make copyright notice consistent with main lib

--- example/src/main/java/nl/littlerobots/example/model/Cheese.java
+++ example/src/main/java/nl/littlerobots/example/model/Cheese.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2015 Hugo Visser (Little Robots)
+ *    Copyright 2015 Little Robots
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
