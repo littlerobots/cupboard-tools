322e447e5b152e9a2b27dcd8ec33c9e7bee8ee27
dylan <dylan@dylans-macbook-pro.local>
2016-04-20 11:27:13 +0200

    Added a module for CupboardDatabase implementation of io.requery's sqlite for android https://github.com/requery/sqlite-android

---
+++ requery-sqlite/src/main/java/nl/littlerobots/cupboard/tools/SQLiteV3CupboardDatabase.java
@@ -0,0 +1,75 @@
+package nl.littlerobots.cupboard.tools;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import io.requery.android.database.sqlite.SQLiteDatabase;
+import nl.qbusict.cupboard.CupboardDatabase;
+
+public class SQLiteV3CupboardDatabase implements CupboardDatabase {
+    private final SQLiteDatabase mDatabase;
+
+    public SQLiteV3CupboardDatabase(SQLiteDatabase mDatabase) {
+        this.mDatabase = mDatabase;
+    }
+
+    @Override
+    public long insertOrThrow(String table, String nullColumnHack, ContentValues values) {
+        return mDatabase.insertOrThrow(table, nullColumnHack, values);
+    }
+
+    @Override
+    public long replaceOrThrow(String table, String nullColumnHack, ContentValues values) {
+        return mDatabase.replaceOrThrow(table, nullColumnHack, values);
+    }
+
+    @Override
+    public int update(String table, ContentValues values, String selection, String[] selectionArgs) {
+        return mDatabase.update(table, values, selection, selectionArgs);
+    }
+
+    @Override
+    public Cursor query(boolean distinct, String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy, String limit) {
+        return mDatabase.query(distinct, table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+    }
+
+    @Override
+    public Cursor rawQuery(String sql, String[] selectionArgs) {
+        return mDatabase.rawQuery(sql, selectionArgs);
+    }
+
+    @Override
+    public int delete(String table, String selection, String[] selectionArgs) {
+        return mDatabase.delete(table, selection, selectionArgs);
+    }
+
+    @Override
+    public boolean inTransaction() {
+        return mDatabase.inTransaction();
+    }
+
+    @Override
+    public void beginTransaction() {
+        mDatabase.beginTransaction();
+    }
+
+    @Override
+    public void yieldIfContendedSafely() {
+        mDatabase.yieldIfContendedSafely();
+    }
+
+    @Override
+    public void setTransactionSuccessful() {
+        mDatabase.setTransactionSuccessful();
+    }
+
+    @Override
+    public void endTransaction() {
+        mDatabase.endTransaction();
+    }
+
+    @Override
+    public void execSQL(String sql) {
+        mDatabase.execSQL(sql);
+    }
+}
